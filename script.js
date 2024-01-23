5bdf5539ae8bee435f809c73e0618a7779ab92a1
jetski.ci <gidrokisik@gmail.com>
2024-01-23 21:36:53 +0500

    aaa

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 const toastBox = document.getElementById("toastBox");
-const successMsg = "Successfully submitted!";
-const errorMsg = "Please fix the error!";
-const invalidMsg = "Invalid input, check again";
+const successMsg =
+  "<i class='fa-regular fa-circle-check'></i> Successfully submitted!";
+const errorMsg =
+  "<i class='fa-regular fa-circle-xmark'></i> Please fix the error!";
+const invalidMsg =
+  "<i class='fa-solid fa-triangle-exclamation'></i> Invalid input, check again";
 
-function showToast() {
+function showToast(msg) {
   const toast = document.createElement("div");
   toast.classList.add("toast");
-  toast.innerHTML = "success";
+  toast.innerHTML = msg;
   toastBox.appendChild(toast);
+
+  if (msg.includes("error")) {
+    toast.classList.add("error");
+  }
+  if (msg.includes("Invalid")) {
+    toast.classList.add("invalid");
+  }
+
+  setTimeout(() => {
+    toast.remove();
+  }, 6000);
 }
